311d2161a5b1a8aed9db45ed0d44e02e64b1e38a
Haldgerd <haldgerd.code@gmail.com>
2021-08-27 12:44:47 +0200

    "Fixed section styles - navigation layouts, added event listeners to buttons - js file."

---
+++ project4-NetflixNvigation/script.js
@@ -0,0 +1,19 @@
+//variables
+const openMenuBtn = document.querySelector(".hamburger__menu");
+const closeMenuBtn = document.querySelector(".hamburger__menu--close");
+const navigationContainers = document.querySelectorAll(".navigation__section");
+console.log(navigationContainers);
+
+openMenuBtn.addEventListener("click", () => {
+  navigationContainers.forEach(container => {
+    console.log(container);
+    container.classList.add("navigation__section--active");
+  })
+});
+
+closeMenuBtn.addEventListener("click",() => {
+  navigationContainers.forEach(container => {
+    console.log(container);
+    container.classList.remove("navigation__section--active");
+  })
+});
