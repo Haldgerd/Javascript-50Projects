948e32308457f586ea25023a656b5dab7ddce024
Haldgerd <haldgerd.code@gmail.com>
2021-08-31 20:53:36 +0200

    "Added funtional javascript code to script.js file. Fixed width of expanding section in style.css file."

--- project5-splitLandingPage/script.js
+++ project5-splitLandingPage/script.js
@@ -1,26 +1,22 @@
 const productContainers = document.querySelectorAll(".product__section");
-
-// productContainers.forEach(container => {
-//   container.addEventListener("mouseenter", () => {
-//     container.parentElement.classList.toggle("product__container--expand");
-//   });
-// });
-
 const sectionLeft =  document.querySelector(".section__left");
 const sectionRight = document.querySelector(".section__right");
 
-// sectionLeft.addEventListener("mouseenter", () => {
-//   sectionLeft.parentElement.classList.add("product__container--expand");
-//   sectionRight.parentElement.classList.add("product__container--shrink");
-// });
 
-// sectionRight.addEventListener("mouseneter", () => {
-//   sectionRight.parentElement.classList.add("product__container--expand");
-//   sectionLeft.parentElement.classList.add("product__container--shrink");
-// })
+sectionLeft.addEventListener("mouseenter", () => sectionLeft.parentElement.classList.add("product__container--expand"));
 
-productContainers.forEach(container => {
-  container.addEventListener("mouseenter", () => {
-    container.classList.add("product__container--expand");
-  });
-});
+sectionLeft.addEventListener("mouseleave", ()=> sectionLeft.parentElement.classList.remove("product__container--expand"));
+
+
+sectionRight.addEventListener("mouseenter", ()=> sectionRight.parentElement.classList.add("product__container--expand"));
+sectionRight.addEventListener("mouseleave", ()=> sectionRight.parentElement.classList.remove("product__container--expand"));
+
+
+ // if (currentContainer.classList.contains("section__left")){
+    //   console.log("heeloo!");
+    //   currentContainer.parentElement.classList.add("product__container--expand");
+    //   sectionRight.parentElement.classList.add("product__container--shrink");
+    // } else {
+    //   sectionRight.parentElement.classList.add("product__container--expand");
+    //   sectionLeft.parentElement.classList.add("product__container--shrink");
+    // }
