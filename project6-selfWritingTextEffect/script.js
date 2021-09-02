9720ab06edba1668e63cdadcb3a02e5bef0ac79a
Haldgerd <haldgerd.code@gmail.com>
2021-09-02 12:44:31 +0200

    "Added functional javascript code that firest automatic text writing."

--- project6-selfWritingTextEffect/script.js
+++ project6-selfWritingTextEffect/script.js
@@ -2,24 +2,39 @@ const textContainer = document.querySelector(".text");
 const speedControl = document.querySelector("input");
 
 let text = "Cyberpunk";
-let counter = 0;
+let counter = 1;
+let speed = 300 / speedControl.value; // so this was the catch. Speed is declared prior to function call.
 
+writeText();
 
-// functions
 function writeText() {
-  textContainer.innerHTML += text[counter];
+  textContainer.innerText = text.slice(0, counter);
   counter++;
 
-  if (counter > text.length) {
-    counter = 0;
+  if(counter > text.length) {
+    counter = 1;
   }
+
+  setTimeout(writeText, speed);
 }
 
-speedControl.addEventListener("input", (e)=> {
-  textContainer.innerHTML = "";
-  let speed = 500 / e.target.value;
-  setTimeout(writeText, speed);
-});
+speedControl.addEventListener("input", (e) => speed = 300 / e.target.value);
+
+// // functions
+// function writeText() {
+//   textContainer.innerHTML += text[counter];
+//   counter++;
+
+//   if (counter > text.length) {
+//     counter = 0;
+//   }
+// }
+
+// speedControl.addEventListener("input", (e)=> {
+//   textContainer.innerHTML = "";
+//   let speed = 500 / e.target.value;
+//   setTimeout(writeText, speed);
+// });
 
 
 
