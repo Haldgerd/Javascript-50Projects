81e47217cca2528a8df834bfdb667619d60507a2
Haldgerd <haldgerd.code@gmail.com>
2021-09-13 12:44:23 +0200

    "Added width animation and opacity animation using scale function. Fixed smaller bugs in style.css file."

--- project9-BlurryLoarder/script.js
+++ project9-BlurryLoarder/script.js
@@ -4,10 +4,39 @@ const containerWidth = document.querySelector(".loading__container");
 console.log(percentage.innerHTML);
 console.log(containerWidth);
 
+let addPercentage = 0; 
+let percentageInterval = setInterval(calcPercentage, 35);
+
+// containerWidth.style.width = `${50}%`;
+
 // version using animate function (animate(keyframes, options) - syntax). Div width changes but not the percentage text.
-window.addEventListener('DOMContentLoaded', () => {
-    containerWidth.animate([
-    {width: "0%"}
-  ], 2500, "linear"
-  );
-});
+// window.addEventListener('DOMContentLoaded', () => {
+//     containerWidth.animate([
+//     {width: "0"}
+//   ], 3200, "linear"
+//   );
+// });
+
+function calcPercentage () {
+  addPercentage++;
+
+  if (addPercentage > 99) {
+    clearInterval(percentageInterval);
+  }
+
+  percentage.innerHTML = `${addPercentage}%`;
+
+  percentage.style.opacity = scale(addPercentage, 0, 100, 1, 0);
+  // containerWidth.style.opacity = scale(addPercentage, 0, 100, 1, 0);
+
+  containerWidth.animate([
+    {width: `${0}%`} 
+  ], 40000, "linear");
+}
+
+
+//https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+// scale function very useful here!
+function scale (number, inMin, inMax, outMin, outMax) {
+  return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
