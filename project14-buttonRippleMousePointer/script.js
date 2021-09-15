854810b0ea893452d13028900280bc2d8dafeb7f
Haldgerd <haldgerd.code@gmail.com>
2021-09-15 21:31:43 +0200

    "Refactored js code."

--- project14-buttonRippleMousePointer/script.js
+++ project14-buttonRippleMousePointer/script.js
@@ -12,7 +12,7 @@ btns.forEach(btn => {
     const btnPositionLeft = btn.offsetLeft;
 
     let clickPosition = getPosition(viewportPositionTop, viewportPositionLeft, btnPositionTop, btnPositionLeft); //function returns array [top-y position, left-x position]
-    let circle = createCircleEffect(clickPosition[0], clickPosition[1]);
+    let circle = createCircle(clickPosition[0], clickPosition[1]);
   
     //append circle effect to btn.
     e.target.appendChild(circle);
@@ -24,6 +24,7 @@ btns.forEach(btn => {
 
 function getPosition(clientTop, clientLeft, btnTop, btnLeft) {
   /**
+  * Return x and y positions of a mouse click relative to button.
  * 
  * @param {number} y position of mouse click reletive to viewport
  * @param {number} x position of mouse click relative to viewport
@@ -40,9 +41,9 @@ function getPosition(clientTop, clientLeft, btnTop, btnLeft) {
 }
 
 
-function createCircleEffect(positionTop, positionLeft) {
+function createCircle(positionTop, positionLeft) {
   /** 
-  // Returns html element with class and it's position.
+  // Returns html element with class and position.
   //
   // @param {number}:  mouse click position on y axis
   // @param {number}: mouse click position on x axis
