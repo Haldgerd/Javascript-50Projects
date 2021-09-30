31b73349dade1b932ce25f4b954708e35b9c61dd
Haldgerd <haldgerd.code@gmail.com>
2021-09-30 13:34:58 +0200

    "Added color to password output element. Added JS code that copies password to user clipboard.  Added code that generates random password. User selection not yet functional."

--- project19-PasswordGeneratorUI/script.js
+++ project19-PasswordGeneratorUI/script.js
@@ -2,23 +2,63 @@ const checkboxes = document.querySelectorAll("input[type=checkbox]");
 
 const passwordLength = document.querySelector("input[type=number]");
 
+const passwordOutput = document.querySelector(".password__generator--output");
 
-// console.log(getRandomLowercase());
+const clipboard = document.querySelector(".password__icon--clipboard");
 
-// console.log(getRandomSymbol());
+const allFunctions = [getRandomLowercase, getRandomUppercase, getRandomSymbol, getRandomNumber];
 
-console.log(passwordLength.value);
+let password = [];
 
-checkboxes.forEach(checkbox => {
-  if (checkbox.checked) {
-    console.log(checkbox.classList);
-    console.log("checked");
+// console.log(passwordOutput);
+// console.log(allFunctions);
+// console.log(allFunctions[0]());
+// console.log(passwordLength.value);
+
+
+getCheckboxes();
+
+if (passwordLength.value){
+
+  for (let initial = 0; initial < passwordLength.value; initial++) {
+
+    let symbol = allFunctions[Math.floor(Math.random() * allFunctions.length)]();
+
+    password.push(symbol);
   }
+
+  password = password.join(""); 
+
+} else {
+  console.log("There is no value.");
+}
+
+console.log(password);
+passwordOutput.textContent = password;
+
+
+clipboard.addEventListener("click", () => {
+alert("Copied to clipboard.");
+ navigator.clipboard.writeText(passwordOutput.innerHTML);
+// //  this doesn't work on IE. Might need to find some fallback code for this.
+// https://stackoverflow.com/questions/33855641/copy-output-of-a-javascript-variable-to-the-clipboard
 });
 
 
-
 // functions
+function getCheckboxes () {
+
+  checkboxes.forEach(checkbox => {
+
+    if (checkbox.checked) {
+
+      console.log(checkbox.classList);
+      // console.log("checked");
+    }
+  });
+}
+
+
 function getRandomLowercase () {
 
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
