aef564e3868ba81a350fdaf2debf01a950e1c11d
Haldgerd <haldgerd.code@gmail.com>
2021-10-03 20:23:59 +0200

    "Added profile images."

---
+++ project20-LiveserFilter/script.js
@@ -0,0 +1,50 @@
+const users = [
+  {
+    image: url("./images/circle-cropped(1).png"),
+    name: "Johanna Strauss",
+    position: "UI/UX designer",
+    location: "Berlin, Germany",
+  },
+  {
+    image: url("./images/circle-cropped.png"),
+    name: "Freya Olmo",
+    position: "People Manager",
+    location: "Malmo, Sweden",
+  },
+  {
+    image: url("./images/circle-cropped(2).png"),
+    name: "Khan Jo",
+    position: "Front end developer",
+    location: "Syndey, Australia",
+  },
+  {
+    image: url("./images/circle-cropped(4).png"),
+    name: "Sashi Paravani",
+    position: "Junior DevOp",
+    location: "Singapur, India",
+  },
+  {
+    image: url("./images/burn-lorna.jpeg"),
+    name: "Rachel Gray",
+    position: "Back end developer",
+    location: "London, UK",
+  },
+   {
+    image: url("./images/cyberpunk.jpeg"),
+    name: "Nika Vidković",
+    position: "graphic designer",
+    location: "Beograd, Serbia",
+  },
+  {
+    image: url("./images/nintendo.jpeg"),
+    name: "Jeff Gregory",
+    position: "SEO specialist",
+    location: "Tallin, Estonia",
+  },
+  {
+    image: url("./images/circle-cropped(3).png"),
+    name: "Monica Gerarro",
+    position: "Project Manager",
+    location: "Verona, Italy",
+  },
+]
