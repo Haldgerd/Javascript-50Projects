599213f9ceb193f540aa90ebee6fbb6b15252b60
Haldgerd <haldgerd.code@gmail.com>
2021-10-08 12:43:09 +0200

    "Added node.js local host, installed axios but uninstalled it due to unproper installation."

---
+++ project21-GitHubAPIProfileSearch/script.js
@@ -0,0 +1,17 @@
+
+//  created local server for fun.
+// const http = require('http');
+
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Are you serious');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+//   console.log("I don't feel smart. Yet.");
+// });
