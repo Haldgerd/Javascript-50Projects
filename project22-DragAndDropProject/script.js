39804839c9de3607bc5087d682bc30689c74971f
Haldgerd <haldgerd.code@gmail.com>
2021-10-11 14:23:29 +0200

    "Drag and drop functionality with javascript. TODO: set container background of draggable target to default color."

--- project22-DragAndDropProject/script.js
+++ project22-DragAndDropProject/script.js
@@ -3,25 +3,41 @@ const dragableImage = document.querySelector(".image__container--active");
 const imageContainers = document.querySelectorAll(".image__container");
 
 
+// event listeners for dragable target
+dragableImage.addEventListener("dragstart", (e)=> {
+  // actions on drag target
+  dragableImage.style.opacity = "0.6";
+  dragableImage.style.border = "3px solid white";
+  // dragableImage.classList.add("drag--active");
+});
+
+dragableImage.addEventListener("dragend", ()=> {
+  dragableImage.style.opacity = "";
+  dragableImage.style.border = "";
+  // setTimeout(() => dragableImage.classList.remove("drag--active"), 10);
+});
+
+
+
 imageContainers.forEach(container => {
-
-  container.addEventListener("drag", ()=> {
-    dragableImage.style.opacity = "0.4";
-    dragableImage.style.border = "3px solid white";
-  });
-
-  container.addEventListener("dragend", ()=> {
-    dragableImage.style.opacity = "1";
-    dragableImage.style.border = "";
-  });
+  // event listeners for dropzones.
 
   container.addEventListener("dragenter", (e) => {
+  // e.preventDefault();
 
   // highlight potential drop target when the draggable element enters it
     e.currentTarget.classList.add("hover--active");
   }); 
 
-  container.addEventListener("dragleave", (e) => {
+
+  /* events fired on the drop targets */
+  container.addEventListener("dragover", (e) => {
+  // prevent default (default doesn't allow drop) to allow drop
+    e.preventDefault();
+  });
+
+  container.addEventListener("dragleave", (e) => { 
+    e.preventDefault();
 
     // reset background of potential drop target when the draggable element leaves itt
       e.currentTarget.classList.remove("hover--active");
@@ -30,10 +46,11 @@ imageContainers.forEach(container => {
 
   container.addEventListener("drop", (e) => {
   // prevent default action (open as link for some elements)
-    e.preventDefault();
-
-    dragableImage.parentNode.removeChild(dragableImage);
+    e.preventDefault;
+   
     e.currentTarget.appendChild(dragableImage);
+
+    e.currentTarget.classList.remove("hover--active");
   });
-  
+
 });
