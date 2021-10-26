6946d4859b42712f341b7530496d56ab099c97fb
Haldgerd <haldgerd.code@gmail.com>
2021-10-26 14:59:10 +0200

    "Added js code to fetch data from 3rd party API. both on DOM load and on button click."

---
+++ project24-JokesAPI/script.js
@@ -0,0 +1,36 @@
+
+const button = document.querySelector("button");
+
+const display = document.querySelector(".joke__container");
+
+const jokeURL = "https://icanhazdadjoke.com"
+
+window.addEventListener("DOMContentLoaded", () => {
+  console.log("fetch random joke");
+  getData();
+
+});
+
+button.addEventListener("click", () => {
+  console.log("fetch a random joke");
+  getData();
+  
+});
+
+function getData() {
+// to fetch data from API that requires some sort of authorization - in our case header use syntax below
+
+  const config = {
+    //headers need to be lowercase , accept wihtout quotes.
+      headers: {
+        Accept: "application/json",
+    },
+  }
+
+  fetch(jokeURL, config)
+    .then((response) => response.json())
+    // .catch(new Error("Error occured fetching the data."))
+    .then((data) => {
+      display.innerHTML = data.joke;
+    });
+}
