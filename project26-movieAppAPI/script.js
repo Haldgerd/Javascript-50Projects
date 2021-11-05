bff8d7a071d4519085bddf4c503f68b0ce3c728e
Haldgerd <haldgerd.code@gmail.com>
2021-11-05 20:33:11 +0100

    "Connected to third party API, modifed structure and art design."

--- project26-movieAppAPI/script.js
+++ project26-movieAppAPI/script.js
@@ -2,6 +2,11 @@ const form = document.querySelector(".search__form");
 
 const input = document.querySelector(".search__input");
 
+const APIurl = "https://api.themoviedb.org/3/discover/network/213&api_key=ad3ffbd0b196e926f7cccabfd2460f2a";
+// https://api.themoviedb.org/3/network/213?api_key=ad3ffbd0b196e926f7cccabfd2460f2a for netflix information
+
+//netflix series by popularity - not netflix but all series by popularity.
+// https://api.themoviedb.org/3/discover/tv?sort_by=popularity&with_network=213&api_key=ad3ffbd0b196e926f7cccabfd2460f2a
 
 window.addEventListener("submit", (e) => {
   e.preventDefault();
